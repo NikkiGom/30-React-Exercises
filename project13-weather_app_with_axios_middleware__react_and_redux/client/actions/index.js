6a49a104314e488bf3e3af6c014624dfa4143daf
Nikki Gomez <nikkigomez@Nikkis-MacBook-Pro.local>
2018-03-02 10:19:14 +0100

    ignore weather app key

--- project13-weather_app_with_axios_middleware__react_and_redux/client/actions/index.js
+++ project13-weather_app_with_axios_middleware__react_and_redux/client/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import {API_KEY} from '../keys.js';
 
-const API_KEY = 'c214fa8010b470efe60a28873c22c0a2';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
